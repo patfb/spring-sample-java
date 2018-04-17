06873343394105d702ae6e874816c5632db27e82
Patrick <patrick.f.boyne@gmail.com>
2018-04-16 22:15:16 -0400

    add property file and use it

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,7 +10,12 @@ public class Application {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		
 		//CustomerService service = new CustomerServiceImpl();
+		
 		CustomerService service = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service);
+		
+		CustomerService service2 = appContext.getBean("customerService", CustomerService.class);
+		System.out.println(service2);
 		
 		System.out.println(service.findAll().get(0).getFirstname());
 	}
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,18 +3,20 @@ package com.pluralsight.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
 @Service("customerService")
+@Scope("prototype") // or @Scope("singleton") for a singleton
 public class CustomerServiceImpl implements CustomerService {
 
 	private CustomerRepository customerRepository;
-	
+
 	public CustomerServiceImpl() {
-		
+
 	}
 
 	public CustomerServiceImpl(CustomerRepository customerRepository) {
