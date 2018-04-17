759fe86a8b4ea49184a91aeee24b99e7e0b86756
Patrick <patrick.f.boyne@gmail.com>
2018-04-16 21:51:56 -0400

    using autowired

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,21 @@
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
-
 @Configuration
+@ComponentScan({ "com.pluralsight" })
 public class AppConfig {
 
-	@Bean(name = "customerService")
+	/*@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
-		return new CustomerServiceImpl();
-	}
+		// CustomerServiceImpl service = new
+		// CustomerServiceImpl(getCustomerRepository());
+		CustomerServiceImpl service = new CustomerServiceImpl();
+		// service.setCustomerRepository(getCustomerRepository());
+		return service;
+	}*/
+
+	/*@Bean(name = "customerRepository")
+	public CustomerRepository getCustomerRepository() {
+		return new HibernateCustomerRepositoryImpl();
+	}*/
 }
--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,10 +3,12 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Customer;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
-
 	
 	@Override
 	public List<Customer> findAll() {
